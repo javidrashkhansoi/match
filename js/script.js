b2a6aa64db9f8d975411ced72824896843d4da53
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-05-05 04:15:41 +0400

    Added main navigation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,61 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
-// import "./window/window.js";
-// import "./scripts/scripts.js";
-// import "./libraries/libraries.js";
 
+;// CONCATENATED MODULE: ./src/js/modules/header-observers.js
+const { documentElement } = document;
+const { classList: htmlClassList, style: htmlStyle } = documentElement;
+
+class HeaderObservers {
+  #$header = document.querySelector("[data-header=\"header\"]");
+  /** @type {HTMLDivElement} */
+  #$headerWrapper = document.querySelector("[data-header=\"wrapper\"]");
+  #addingClass = "scrolled";
+  #cssProperty = "--header-height";
+  #intersection;
+  #resize;
+
+  /** @param {HeaderObserversOptions} options */
+  constructor(options = {}) {
+    this.#intersection = options.intersection ?? true;
+    this.#resize = options.resize ?? true;
+
+    this.#init();
+  }
+
+  #init() {
+    if (this.#intersection && this.#$header) {
+      const intersectionObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          htmlClassList.toggle(this.#addingClass, !entry.isIntersecting);
+        });
+      });
+
+      intersectionObserver.observe(this.#$header);
+    }
+
+    if (this.#resize && this.#$headerWrapper) {
+      const resizeObserver = new ResizeObserver(entries => {
+        entries.forEach(entry => {
+          const { borderBoxSize: [{ blockSize }] } = entry;
+
+          htmlStyle.setProperty(this.#cssProperty, `${blockSize}px`);
+        });
+      });
+
+      resizeObserver.observe(this.#$headerWrapper);
+    }
+  }
+}
+
+
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/header-observers.js
+
+
+const headerObservers = new HeaderObservers();
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/find-filter.js
 /** @type {HTMLButtonElement} */
 const findFilterButton = document.querySelector(".filter-button");
 /** @type {HTMLFormElement} */
@@ -15,6 +66,17 @@ if (findFilterButton && findFilter) {
     findFilter.classList.add("find-form--show");
   });
 }
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts.js
+
+
+// import "./scripts/burger.js";
+// import "./scripts/range.js";
+
+;// CONCATENATED MODULE: ./src/js/script.js
+
+// import "./window/window.js";
+// import "./libraries/libraries.js";
 
 /******/ })()
 ;
